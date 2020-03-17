21cd1e8f7f70cdf4c99b1c843b0220db136b1789
davideUNICAM <davide.angelani@studenti.unicam.it>
2020-03-17 11:36:10 +0100

    prepare https

--- server/server.js
+++ server/server.js
@@ -1,8 +1,17 @@
-const express = require('express');
+
+// const http = require('http');
+// const https = require('https');
 const bodyParser = require('body-parser');
 const path = require('path');
 const db = require ('./app/models/index');
 const cors = require('cors');
+// const fs = require('fs');
+// const credentials = {
+//   key: fs.readFileSync('key.pem'),
+//   cert: fs.readFileSync('cert.pem')
+// };
+
+const express = require('express');
 const app = express();
 const routes = require('./app/routes/routes');
 const PORT = 3000;
@@ -13,6 +22,11 @@ app.use(bodyParser.json());
 routes(app);
 app.use(cors());
 app.use(express.static('public/'));
+
+// const server = https.createServer(credentials, app);
+// server.listen(PORT, () => {
+//   console.log(`App listening on PORT ${PORT}`);
+// });
 //db.sequelize.sync({ force: true }).then(() => {
   app.listen(PORT, () => {
     console.log(`App listening on PORT ${PORT}`);
