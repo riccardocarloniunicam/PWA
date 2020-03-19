8c653dbb8ecd971b2b239881219f3ce212146a03
davideUNICAM <davide.angelani@studenti.unicam.it>
2020-03-19 11:40:01 +0100

    adding https wip

--- server/server.js
+++ server/server.js
@@ -28,7 +28,7 @@ app.use(express.static('public/'));
 //   console.log(`App listening on PORT ${PORT}`);
 // });
 //  db.sequelize.sync({ force: true }).then(() => {
-//   app.listen(PORT, () => {
-//     console.log(`App listening on PORT ${PORT}`);
-//   });
+  app.listen(PORT, () => {
+    console.log(`App listening on PORT ${PORT}`);
+  });
 //  });
